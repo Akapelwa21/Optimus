dbc667acb741a7a4af7dead8741ea5b50e6ddb38
Richard Akapelwa <akapelwaa@gmail.com>
2022-12-31 12:15:06 +0100

    update the server url

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5173, () => console.log('AI server started on https://localhost5173'))
+app.listen(5000, () => console.log('AI server started on https://localhost5000'))
