4bbd0256c47a3a2c06d667e549d2c203a22cd4de
Richard Akapelwa <akapelwaa@gmail.com>
2022-12-31 12:50:28 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://optimus-5z7j.onrender.com', {
+    const response = await fetch('https://optimus-5z7j.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
